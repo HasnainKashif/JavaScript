a3e472d95df7d7eb9cc473fdd9bb33e7683f92d3
Hasnain Kashif <hkashif1111@gmail.com>
2023-08-01 23:05:18 -0700

    first commit

---
+++ work.js
@@ -0,0 +1,9 @@
+var names = prompt("Enter Your Name");
+var mathsNumber = +prompt("Enter Your Number");
+var physicsNumber = +prompt("Enter Your Number");
+var chemsitryNumber = +prompt("Enter Your Number");
+var computerNumber = +prompt("Enter Your Number");
+var totalNumber = (650);
+var studentTotalMarks = (mathsNumber + physicsNumberNumber + chemsitryNumberNumber + computerNumber);
+var studentPercantage = (studentTotalMarks*100/totalNumber);
+console.log(studentPercantage);
